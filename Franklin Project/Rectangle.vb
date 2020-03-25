659b8b46a78616209355f3f147dd8f4f253af840
Ian Skelskey <ianskelskey@gmail.com>
2020-03-25 13:10:49 -0700

    Added Rectangle Class

---
+++ Franklin Project/Rectangle.vb
@@ -0,0 +1,38 @@
+﻿Public Class Rectangle
+    Private intLength As Integer
+    Private intWidth As Integer
+    Public Property Length As Integer
+        Get
+            Return intLength
+        End Get
+        Set(value As Integer)
+            If intLength > 0 Then
+                Length = intLength
+            Else
+                Length = 0
+            End If
+        End Set
+    End Property
+
+    Public Property Width As Integer
+        Get
+            Return intWidth
+        End Get
+        Set(value As Integer)
+            If intWidth > 0 Then
+                Width = intWidth
+            Else
+                Width = 0
+            End If
+        End Set
+    End Property
+
+    Public Sub New()
+        intLength = 0
+        intWidth = 0
+    End Sub
+
+    Public Function GetArea() As Integer
+        Return intLength * intWidth
+    End Function
+End Class
